47f007769291c75246bf0e950e14be35ba9d1cf8
clami66 <claudio.mirabello@scilifelab.se>
2025-03-24 16:02:33 +0100

    pdbdata

--- alphafold/data/mmcif_parsing.py
+++ alphafold/data/mmcif_parsing.py
@@ -258,7 +258,7 @@ def parse(*,
       author_chain = mmcif_to_author_chain_id[chain_id]
       seq = []
       for monomer in seq_info:
-        code = SCOPData.protein_letters_3to1.get(monomer.id, 'X')
+        code = PDBData.protein_letters_3to1.get(monomer.id, 'X')
         seq.append(code if len(code) == 1 else 'X')
       seq = ''.join(seq)
       author_chain_to_sequence[author_chain] = seq
